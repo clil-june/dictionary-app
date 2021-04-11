d05c91ad0447cc853e5a71ee006c6fd2c0877faf
Clil SneOR <clil.s.au@gmail.com>
2021-04-11 15:35:28 +0300

    built a search engine

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Content.css";
 
 export default function Content(){
+  let [word, setWord]=useState("");
+
+  function search(event){
+    event.preventDefault();
+    console.log(word);
+  }
+
+  function handleClick(event){
+    setWord(event.target.value);
+  }
     return (
-        <div calssName="Content">
+        <div className="Content">
             <h1>
                 Dictionary
             </h1>
-            <form id="search-form" autoComplete="off">
-                <input type="search" placeholder="Type any word" id="search-bar" />
+            <form id="search-form" autoComplete="off" onClick={search}>
+                <input type="search" placeholder="Type any word" id="search-bar" onClick={handleClick} />
                 <input type="submit" value="search" id="search" />
             </form>
+            <h2>{word}</h2>
         </div>
     );
 }
