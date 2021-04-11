85d522af89c757bdf5081f8837ea1df3f222f320
Clil SneOR <clil.s.au@gmail.com>
2021-04-11 14:13:21 +0300

    built search bar and styled page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Content from "./Content";
+import "./App.css";
 
 export default function App(){
-  return(<Content />);
+  return(
+    <div className="dictionary">
+  <Content />
+  </div>
+  );
   
 }
--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./Content.css";
 
 export default function Content(){
     return (
         <div calssName="Content">
-            Dictionary
+            <h1>
+                Dictionary
+            </h1>
+            <form id="search-form" autoComplete="off">
+                <input type="search" placeholder="Type any word" id="search-bar" />
+                <input type="submit" value="search" id="search" />
+            </form>
         </div>
     );
 }
