62a8382ad1426e705e700a7010c43056f7fadfe5
Clil SneOR <clil.s.au@gmail.com>
2021-04-12 09:55:36 +0300

    installed axios and integrated dictionary api

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Content.css";
 
 export default function Content(){
   let [word, setWord]=useState("");
 
+  function handleResponse(response){
+    console.log(response.data);
+  }
+
   function search(event){
     event.preventDefault();
     alert(`searching forvthe defenition of ${word}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`
+    axios.get(apiUrl).then(handleResponse);
   }
 
-  function handleClick(event){
+  function handleChange (event){
     setWord(event.target.value);
   }
     return (
@@ -18,8 +25,7 @@ export default function Content(){
                 Dictionary
             </h1>
             <form id="search-form" autoComplete="off" onSubmit={search} >
-                <input type="search" placeholder="Type any word" id="search-bar" onChange={handleClick} />
-                <input type="submit" value="search" id="search" />
+                <input type="search" placeholder="Type any word" id="search-bar" onChange={handleChange} />
             </form>
         </div>
     );
