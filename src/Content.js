9fce0af0feb54289810a686892257f19e7e332d6
Clil SneOR <clil.s.au@gmail.com>
2021-04-21 11:48:09 +0300

    added an illustration and set default word on load

--- src/Content.js
+++ src/Content.js
@@ -2,34 +2,52 @@ import React, {useState} from "react";
 import axios from "axios";
 import Definition from "./Definition";
 import "./Content.css";
+import reading from "./reading.svg";
 
 export default function Content(props){
   let [word, setWord]=useState("joy");
   let [results, setResults]=useState(null);
+  let [loaded, setLoaded]=useState(false);
 
   function handleResponse(response){
     setResults(response.data[0]);
     
   }
 
-  function search(event){
-    event.preventDefault();
+  function search(){
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_GB/${word}`
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    search();
   }
 
   function handleChange (event){
     setWord(event.target.value);
   }
+
+  function load(){
+    setLoaded(true);
+    search();
+  }
+  if (loaded){
     return (
         <div className="Content">
             <h1>
                 Dictionary
             </h1>
-            <form id="search-form" autoComplete="off" onSubmit={search} >
+            <form id="search-form" autoComplete="off" onSubmit={handleSubmit} >
                 <input type="search" placeholder="Type any word" id="search-bar" onChange={handleChange} />
             </form>
+            <img className="reading" src={reading} />
             <Definition results={results} />
         </div>
-    );
+    );}
+    else{
+      load();
+      return null;
+    }
+  
 }
