3c628149adf08ef6099b26c3837c5676295a0335
Clil SneOR <clil.s.au@gmail.com>
2021-04-29 18:06:25 +0300

    added a link to my linkedin profile

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <footer><a href="https://github.com/clil-june/dictionary-app" targer="_blank" rel="noreferrer" className="footer-link">Open source code</a> by Clil</footer>
+        <footer><a href="https://github.com/clil-june/dictionary-app" targer="_blank" rel="noreferrer" className="footer-link">Open source code</a> by <a href="https://www.linkedin.com/in/clil-sne-or-128153211/" targer="_blank" rel="noreferrer">Clil</a></footer>
     );
 }
